a773ea715e9be56436cffd3d305f60471fe2a6c6
Mr.GolDi <marachckow2013@yandex.ru>
2021-07-11 23:25:00 +0300

    s

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/demo-deploy'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
+  res.sendFile(path.join(__dirname+'/united/src/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
